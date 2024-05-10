fe1a999fd9909f7b0baf98969d239a41cd3dd028
Luis Paulo <luispaulolpsn@gmail.com>
2024-05-10 10:22:52 -0300

    incluindo documentacao swagger

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -7,6 +7,30 @@ use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
 
+/**
+ * @OA\SecurityScheme(
+ *     securityScheme="bearerAuth",
+ *     type="http",
+ *     scheme="bearer",
+ *     bearerFormat="JWT",
+ * )
+ */
+/**
+ * @OA\Info(
+ *      version="1.0.0",
+ *      title="API Documentation",
+ *      description="Description removed for better illustration of structure.",
+ *      @OA\Contact(
+ *          email="admin@example.test",
+ *          name="Noticias",
+ *          url="https://google.com.br"
+ *      ),
+ *      @OA\License(
+ *          name="Apache 2.0",
+ *          url="http://www.apache.org/licenses/LICENSE-2.0.html"
+ *      )
+ * )
+ */
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
--- app/Http/Controllers/NoticiaController.php
+++ app/Http/Controllers/NoticiaController.php
@@ -7,11 +7,37 @@ use App\Models\Noticia;
 
 class NoticiaController extends Controller
 {
+
+    /**
+     * @OA\Get(
+     *     path="/api/noticias",
+     *     tags={"Noticias"},
+     *     summary="Retorna todas as noticias",
+     *     @OA\Response(response="200", description="Sucesso")
+     * )
+     */
     public function index()
     {
         return Noticia::all();
     }
 
+    /**
+     * @OA\Post(
+     *     path="/api/noticias",
+     *     tags={"Noticias"},
+     *     summary="Cria uma nova notícia",
+     *     @OA\RequestBody(
+     *         required=true,
+     *         @OA\JsonContent(
+     *             required={"titulo", "descricao"},
+     *             @OA\Property(property="titulo", type="string", example="Título da Notícia"),
+     *             @OA\Property(property="descricao", type="string", example="Descrição da Notícia")
+     *         ),
+     *     ),
+     *     @OA\Response(response="200", description="Notícia criada com sucesso"),
+     *     @OA\Response(response="422", description="Erro de validação")
+     * )
+     */
     public function store(Request $request)
     {
         $request->validate([
@@ -22,11 +48,56 @@ class NoticiaController extends Controller
         return Noticia::create($request->all());
     }
 
+    /**
+     * @OA\Get(
+     *     path="/api/noticias/{id}",
+     *     tags={"Noticias"},
+     *     summary="Retorna uma notícia específica",
+     *     @OA\Parameter(
+     *         name="id",
+     *         in="path",
+     *         required=true,
+     *         @OA\Schema(
+     *             type="integer",
+     *             format="int64"
+     *         )
+     *     ),
+     *     @OA\Response(response="200", description="Sucesso"),
+     *     @OA\Response(response="404", description="Notícia não encontrada")
+     * )
+     */
     public function show($id)
     {
         return Noticia::findOrFail($id);
     }
 
+    /**
+     * @OA\Put(
+     *     path="/api/noticias/{id}",
+     *     tags={"Noticias"},
+     *     summary="Atualiza uma notícia existente",
+     *     @OA\Parameter(
+     *         name="id",
+     *         in="path",
+     *         required=true,
+     *         @OA\Schema(
+     *             type="integer",
+     *             format="int64"
+     *         )
+     *     ),
+     *     @OA\RequestBody(
+     *         required=true,
+     *         @OA\JsonContent(
+     *             required={"titulo", "descricao"},
+     *             @OA\Property(property="titulo", type="string", example="Novo Título da Notícia"),
+     *             @OA\Property(property="descricao", type="string", example="Nova Descrição da Notícia")
+     *         ),
+     *     ),
+     *     @OA\Response(response="200", description="Notícia atualizada com sucesso"),
+     *     @OA\Response(response="404", description="Notícia não encontrada"),
+     *     @OA\Response(response="422", description="Erro de validação")
+     * )
+     */
     public function update(Request $request, $id)
     {
         $noticia = Noticia::findOrFail($id);
@@ -35,6 +106,24 @@ class NoticiaController extends Controller
         return $noticia;
     }
 
+    /**
+     * @OA\Delete(
+     *     path="/api/noticias/{id}",
+     *     tags={"Noticias"},
+     *     summary="Deleta uma notícia existente",
+     *     @OA\Parameter(
+     *         name="id",
+     *         in="path",
+     *         required=true,
+     *         @OA\Schema(
+     *             type="integer",
+     *             format="int64"
+     *         )
+     *     ),
+     *     @OA\Response(response="200", description="Notícia deletada com sucesso"),
+     *     @OA\Response(response="404", description="Notícia não encontrada")
+     * )
+     */
     public function destroy($id)
     {
         $noticia = Noticia::findOrFail($id);
