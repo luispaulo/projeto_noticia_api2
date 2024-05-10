a3b53fa078af4155d446e43e415f78f43b5b2d27
Luis Paulo <luispaulolpsn@gmail.com>
2024-05-10 13:58:34 -0300

    modificando README

--- routes/api.php
+++ routes/api.php
@@ -19,10 +19,6 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
 Route::get('/noticias', [NoticiaController::class, 'index']);
 Route::post('/noticias', [NoticiaController::class, 'store']);
 Route::get('/noticias/{id}', [NoticiaController::class, 'show']);
